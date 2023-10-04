3df8699476ee855420001d4c35c604a4d903ac0d
nikhil2060 <kumarnikhil2060@gmail.com>
2023-10-04 16:35:16 +0530

    search implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import carData from "./sampleData.json";
+import CarCard from "./carCard";
+import { useState } from "react";
+import { MagnifyingGlass } from "@phosphor-icons/react";
 
-function App() {
+export default function App() {
+  const [searchItem, setSearchItem] = useState("");
+
+  function handleSeachRender(item) {
+    console.log("hello");
+    setSearchItem(item);
+  }
+
+  if (searchItem !== "") {
+    return (
+      <div className="main-container">
+        <div className="search-container">
+          <Search search={handleSeachRender} />
+        </div>
+        <div className="list-container">
+          <ul className="list">
+            <CarCard car={searchItem} />
+          </ul>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="search-container">
+        <Search search={handleSeachRender} />
+      </div>
+      <div className="list-container">
+        <ul className="list">
+          {carData.cars.map((car, index) => (
+            <li key={index}>
+              <CarCard car={carData.cars[index]} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
 
-export default App;
+function Search({ search }) {
+  const [item, setItem] = useState("");
+
+  function searchCarByName(carName) {
+    carName = carName.toLowerCase(); // Convert the search query to lowercase for case-insensitive search
+    const results = carData.cars.filter((car) =>
+      car.car_name.toLowerCase().includes(carName)
+    );
+
+    return results[0];
+  }
+
+  const handleInputChange = (event) => {
+    setItem(event.target.value);
+  };
+
+  const handleSearchClick = () => {
+    const ans = searchCarByName(item);
+    if (!ans) return;
+    search(ans);
+  };
+
+  return (
+    <div className="search-inside-box">
+      <input
+        type="text"
+        placeholder="Search"
+        className="search-input"
+        onChange={handleInputChange}
+      />
+      <button onClick={handleSearchClick}>
+        <MagnifyingGlass size={20} color="#555" />
+      </button>
+    </div>
+  );
+}
---
+++ src/carCard.js
@@ -0,0 +1,59 @@
+import "./carCard.css";
+import {
+  Users,
+  GasPump,
+  Gauge,
+  SteeringWheel,
+  Heart,
+} from "@phosphor-icons/react";
+
+export default function CarCard({ car }) {
+  const image = car.photo;
+  return (
+    <div className="car-card">
+      <img src={car.photo} alt="camry" className="car-image" />
+      <div className="car-details">
+        <p className="car-title">{car.car_name}</p>
+        <div className="car-att">
+          <CarInfo
+            icon={<Users size={29} color="#189cf4" />}
+            value={`${car.seating_cap} People`}
+          />
+          <CarInfo
+            icon={<GasPump size={29} color="#189cf4" />}
+            value={car.fuel_type}
+          />
+          <CarInfo
+            icon={<Gauge size={29} color="#189cf4" />}
+            value={`${car.mileage} Km / 1-litre`}
+          />
+          <CarInfo
+            icon={<SteeringWheel size={29} color="#189cf4" />}
+            value={car.transmission}
+          />
+        </div>
+        <div className="price-box">
+          <p className="rent">
+            $ {+car.rent_price * 15}
+            <span> / month</span>
+          </p>
+          <div className="btns">
+            <button className="btn-like">
+              <Heart size={20} color="#189cf4" />
+            </button>
+            <button className="btn-rent">Rent Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function CarInfo({ icon, value }) {
+  return (
+    <span className="car-people">
+      {icon}
+      <span>{value}</span>
+    </span>
+  );
+}
